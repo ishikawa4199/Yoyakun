c501939ed3cc7bd3bf8f1e74ac066c2b9e96cebb
taatan <ishitatsuya519@yahoo.co.jp>
2023-10-05 16:27:31 +0900

    厨房注文リスト完成

---
+++ public/js/orderList.js
@@ -0,0 +1,32 @@
+setInterval(function(){
+    $(function(){
+        $.ajax({
+            type:"get",
+            url:"/cook/get/orders",
+            dataType:"json"
+        }).done((res)=>{
+            
+            $('.order-list-container').find('.order-list').remove();
+            $.each(res,function(index,value){
+                
+                
+                html = `
+                <div class="order-list">
+                <h2>商品名:${value.name}</h2>
+                <h2>数量:${value.quantity}</h2>
+                <h3>シート番号:${value.seat_num}</h3>
+                <a href="/slip/list/${value.slip_num}">伝票番号:${value.slip_num}</a>
+                        <a href="">レシピを見る</a>
+                        </div>
+                        `
+                        
+                $('.order-list-container').append(html);
+                    });
+                    
+                }).fail((error)=>{
+            console.log(error);
+        });
+    });
+
+
+},2000);
---
+++ resources/views/start.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+
+@section('content')
+<form action="{{ route('setting.store') }}" method="post">
+    @csrf
+    <h3>ご利用人数を入力してください。</h3>
+    <input type="text" name="person_count">
+    <h3>テーブル番号を入力してください。</h3>
+    <input type="text" name="seat_num">
+
+    <input type="submit" name="btn" value="決定">
+
+    
+    
+
+</form>
+
+
+
+
+@endsection
